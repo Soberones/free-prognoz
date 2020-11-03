8f25a293cb149667771a192711415678c970ac12
Serhei Sobal <serhei.sobal@gmail.com>
2020-11-03 11:04:41 +0300

    fix and seo futures

--- components/more-stories.js
+++ components/more-stories.js
@@ -1,10 +1,10 @@
-import PostPreview from '../components/post-preview'
+import PostPreview from "../components/post-preview";
 
 export default function MoreStories({ posts }) {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+        Ещё прогнозы
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
         {posts.map(({ node }) => (
@@ -13,12 +13,10 @@ export default function MoreStories({ posts }) {
             title={node.title}
             coverImage={node.featuredImage?.node}
             date={node.date}
-            author={node.author?.node}
             slug={node.slug}
-            excerpt={node.excerpt}
           />
         ))}
       </div>
     </section>
-  )
+  );
 }
--- components/post-header.js
+++ components/post-header.js
@@ -1,8 +1,8 @@
-import Avatar from '../components/avatar'
-import Date from '../components/date'
-import CoverImage from '../components/cover-image'
-import PostTitle from '../components/post-title'
-import Categories from '../components/categories'
+import Avatar from "../components/avatar";
+import Date from "../components/date";
+import CoverImage from "../components/cover-image";
+import PostTitle from "../components/post-title";
+import Categories from "../components/categories";
 
 export default function PostHeader({
   title,
@@ -14,9 +14,9 @@ export default function PostHeader({
   return (
     <>
       <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
+      {/* <div className="hidden md:block md:mb-12">
         <Avatar author={author} />
-      </div>
+      </div> */}
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} coverImage={coverImage} />
       </div>
@@ -25,10 +25,10 @@ export default function PostHeader({
           <Avatar author={author} />
         </div>
         <div className="mb-6 text-lg">
-          Posted <Date dateString={date} />
+          Опубликовано <Date dateString={date} />
           <Categories categories={categories} />
         </div>
       </div>
     </>
-  )
+  );
 }
