70ad1f6b501a1de198d9bad3e37ddf0ba7d07a41
Serhei Sobal <serhei.sobal@gmail.com>
2020-11-18 12:21:40 +0300

    design fix

--- components/alert.js
+++ components/alert.js
@@ -1,17 +1,17 @@
-import Container from './container'
-import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from "./container";
+import cn from "classnames";
+import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Alert({ preview }) {
   return (
     <div
-      className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
+      className={cn("border-b", {
+        "bg-accent-7 border-accent-7 text-white": preview,
+        "bg-accent-1 border-accent-2": !preview,
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
+        {/* <div className="py-2 text-center text-sm">
           {preview ? (
             <>
               This is page is a preview.{' '}
@@ -35,8 +35,8 @@ export default function Alert({ preview }) {
               .
             </>
           )}
-        </div>
+        </div> */}
       </Container>
     </div>
-  )
+  );
 }
